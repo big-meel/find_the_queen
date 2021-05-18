996f7454792e6d4277277553599a4a128c2b318c
big-meel <big.meel@zoho.com>
2021-05-18 04:14:31 -0500

    Add authentication for clients

--- client.rb
+++ client.rb
@@ -2,26 +2,32 @@ require 'socket'
 
 class Client
   def initialize(addr, port)
-    @socket = TCPSocket.open("localhost", 7621) # TODO: Substitute for env variables
+    @socket = TCPSocket.new("localhost", 7621) # TODO: Substitute for env variables
 
-    # @request = send_request
-    # @response = listen_response
+    @request = send_request
+    @response = listen_response
 
-    # @request.join
-    # @response.join
-
-
-    while line = @socket.gets
-      # line.chop
-      puts line
-    end
+    @request
+    @response.join
   end
 
   def send_request
-
+    puts "Please Enter username and password to connect: "
+    Thread.new do
+      loop do
+        message = $stdin.gets.chomp
+        @socket.puts message
+      end
+    end
   end
 
   def listen_response
+    Thread.new do
+      loop do
+        response = @socket.gets.chomp
+        puts "#{response}"
+      end
+    end
   end
 end
 
--- server.rb
+++ server.rb
@@ -2,9 +2,10 @@
 require 'socket'
 
 class GameServer
-  def initialize(addr, port)
+  def initialize(addr, port, authorized_clients)
     @server_socket = TCPServer.open(addr, port)
 
+    @authorized_clients = authorized_clients
 
     puts 'Starting Server.....'
     run_server
@@ -15,14 +16,30 @@ class GameServer
     loop {
       client = @server_socket.accept
       Thread.start(client) do |client|
+
+        client.puts "Username: "
+        uname = client.gets.chomp
+
+
+        client.puts "Password: "
+        pword = client.gets.chomp
+
+        login = { uname.to_s => pword.to_s }
+        unless @authorized_clients.include? login
+          client.puts "Login credentials incorrect...."
+          client.puts "Closing connection"
+          client.kill self
+        end
+
+
         client.puts "#{client} is Connected"
-        client.close
+        # client.close
       end
-    }
+    }.join
   end
 
 
 end
 
-
-GameServer.new("localhost", 7621)
+authorized_clients = [ {"dannyboi" => "dre@margh_shelled"}, {"matty7" => "win&win99"} ]
+GameServer.new("localhost", 7621, authorized_clients )
