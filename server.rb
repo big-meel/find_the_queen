0ad999f53284c08bbb4389d47b4d39f0276d191f
big-meel <big.meel@zoho.com>
2021-05-18 05:03:58 -0500

    Add conditions for login

--- server.rb
+++ server.rb
@@ -7,6 +7,8 @@ class GameServer
 
     @authorized_clients = authorized_clients
 
+    @players = Hash.new
+
     puts 'Starting Server.....'
     run_server
   end
@@ -25,15 +27,30 @@ class GameServer
         pword = client.gets.chomp
 
         login = { uname.to_s => pword.to_s }
-        unless @authorized_clients.include? login
-          client.puts "Login credentials incorrect...."
+        unless @authorized_clients.include?(login) && @players.keys.count < 2
+          client.puts "Cannot Login..."
           client.puts "Closing connection"
-          client.kill self
+          client.kill self             # TODO: Find replacement method for kill
+        else
+          if @players.keys.include? uname.to_sym
+            client.puts "User is already logged in"
+            client.puts "Closing connection"
+            client.kill self
+          else
+            @players[uname.to_sym] = []
+          end
         end
 
-
+        
+        
+        
         client.puts "#{client} is Connected"
+        client.puts "#{@players.keys.count} players "
         # client.close
+        
+        if @players.keys.count == 2
+          client.puts "Lets Begin"
+        end
       end
     }.join
   end
@@ -41,5 +58,5 @@ class GameServer
 
 end
 
-authorized_clients = [ {"dannyboi" => "dre@margh_shelled"}, {"matty7" => "win&win99"} ]
+authorized_clients =  [ {"dannyboi" => "dre@margh_shelled"}, {"matty7" => "win&win99" }] 
 GameServer.new("localhost", 7621, authorized_clients )
